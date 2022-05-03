ff49c41e1973bbd61f4843cef0e0cd09afd3c93f
Justin Yi <jlyi@princeton.edu>
2022-05-03 18:01:40 -0400

    game

---
+++ src/components/objects/Game/Game.js
@@ -0,0 +1,23 @@
+
+class Game {
+    constructor(parent){
+        this.parent = parent;
+        this.state{
+            gui: parent.state.gui,
+            yellow_remain = 12,
+            blue_remain = 12,
+            game_over = false,
+            ball_instances = 0,
+        }
+        parent.addToUpdateList(this);
+    }
+    rerack(rack){
+        if
+    }
+
+    update(){
+        if(this.parent.gui.)
+    }
+}
+
+export default Game;
